2b10fe88dc0e5ab2d20fa8bccbf3ee3c34f08347
Nicolas Grekas <nicolas.grekas@gmail.com>
2024-01-17 12:22:36 +0100

    up to akismet client

--- src/Controller/ConferenceControler.php
+++ src/Controller/ConferenceControler.php
@@ -2,16 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use App\SpamChecker;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceControler extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+
+        #[Autowire(param: 'photo_dir')]
+        private string $photoDir,
+
+        private SpamChecker $spamChecker,
+    ) {
+
+    }
+
     #[Route('/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
@@ -22,19 +41,52 @@ class ConferenceControler extends AbstractController
 
     #[Route('/conference/{slug}', name: 'conference')]
     public function show(
+        Request $request,
         Conference $conference,
         CommentRepository $commentRepository,
         #[MapQueryParameter(options: ['min_range' => 0])]
         int $offset = 0,
     ): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                $photo->move($this->photoDir, $filename);
+                $comment->setPhotoFilename($filename);
+            }
+
+            $this->entityManager->persist($comment);
+
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+
+            if (2 === $this->spamChecker->getSpamScore($comment, $context)) {
+                throw new \RuntimeException('Blatant spam, go away!');
+            }
+
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
 
         return $this->render('conference/show.html.twig', [
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form,
         ]);
     }
 }
